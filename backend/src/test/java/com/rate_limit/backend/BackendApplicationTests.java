1524bf8fcb0880115aea1777d07156b6d3217e19
Onkar265 <jonkar2003@gmail.com>
2026-08-21 16:49:19 +0530

    project created

---
+++ backend/src/test/java/com/rate_limit/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rate_limit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
