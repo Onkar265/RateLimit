0a08d0cebc71581fcf5f74f2e1e4184dcfabc8be
Onkar265 <jonkar2003@gmail.com>
2026-08-27 15:02:48 +0530

    RedisScriptConfig implemented

--- backend/src/main/java/com/rate_limit/backend/config/RedisScriptConfig.java
+++ backend/src/main/java/com/rate_limit/backend/config/RedisScriptConfig.java
@@ -1,5 +1,16 @@
 package com.rate_limit.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.RedisScript;
+
+@Configuration
 public class RedisScriptConfig {
     
+    @Bean
+    public RedisScript<Long> tockenBucketScript()
+    {
+        return RedisScript.of(new ClassPathResource("scripts/.tokenBucket.lua"),Long.class);
+    }
 }
